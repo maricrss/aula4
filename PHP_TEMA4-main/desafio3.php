9d05a763a8403c2f1a62de9ab4d2e11be50afd06
maricrss <mclararibeirosev@gmail.com>
2024-10-24 17:42:10 -0300

    Add files via upload

---
+++ PHP_TEMA4-main/desafio3.php
@@ -0,0 +1,13 @@
+ <?php
+/*$x = 501;
+
+do {
+    echo "O valor de x 5x <br>";
+    $x+=2;
+} while ($x <= 1001);*/
+
+for($i = 501; $i <=1001; $i+=2){
+    echo "<li class='number'>$i</li>";
+}
+
+?>
---
+++ PHP_TEMA4-main/while.php
@@ -0,0 +1,9 @@
+<?php
+$x = 5;
+
+while ($x < 5) {
+    echo "O valor de x é: $x <br>";
+    $x++;
+}
+?>
+
